031166f6df72490f7ec164fec7e6881c6127c3e1
Pedro Callil <pedrocallil@usp.br>
2025-01-31 05:32:11 -0300

    New (and more correct) calculation for global warming potential.

--- flare_intensity.py
+++ flare_intensity.py
@@ -6,16 +6,13 @@ from field_gas_profiles import flared_components
 
 def get_flare_intensity(volume, fractions):
 
-    volume_scf = 1e6*volume/0.3048**3
-    moles_of_carbon = 0
+    volume_scf = volume
     flared_vol = 0
-    for i in range(len(components)):
+    moles_of_carbon = 1.48*1.1956*volume_scf*fractions[0]/sum(fractions)
+    for i in range(1,len(components)):
         moles_of_carbon += carbons_in_components[i]*\
                             1.1956*\
                             volume_scf*\
                             fractions[i]/sum(fractions)
-        flared_vol += flared_components[i]*\
-                            volume*\
-                            fractions[i]/sum(fractions)
-    flared_mass = moles_of_carbon*0.044009*0.001
-    return flared_mass, flared_vol
+    flared_mass = moles_of_carbon*0.044009*0.001*0.365
+    return flared_mass
